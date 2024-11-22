f23c03ab6b96cb9520eb15b7d55f58b302c6d77a
zaminalili <zaminalili29@gmail.com>
2024-11-22 22:43:52 +0400

    Create db context

---
+++ shorturl.API/Models/ShorturlDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shorturl.API.Models.Entities;
+
+namespace shorturl.API.Models;
+
+internal class ShorturlDbContext(DbContextOptions<ShorturlDbContext> options): DbContext (options)
+{
+    internal DbSet<Url> Urls {  get; set; }
+}
