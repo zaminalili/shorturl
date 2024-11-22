9fb8c937df5439938bf63e9ec67e373f60bb7029
zaminalili <zaminalili29@gmail.com>
2024-11-22 22:25:02 +0400

    Create url entity

---
+++ shorturl.API/Models/Entities/Url.cs
@@ -0,0 +1,11 @@
+﻿namespace shorturl.API.Models.Entities;
+
+public class Url
+{
+    public int Id { get; set; }
+    public string OriginalUrl { get; set; } = default!;
+    public string ShortCode { get; set; } = default!;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public int AccessCount { get; set; }
+}
