d2c3ae34c18e4dbfa1caade3c77f389c29779b68
zaminalili <zaminalili29@gmail.com>
2024-11-23 17:08:44 +0400

    Add AutoMapper

---
+++ shorturl.API/Mappings/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using shorturl.API.Models.Dtos;
+using shorturl.API.Models.Entities;
+
+namespace shorturl.API.Mappings;
+
+public class MappingProfile: Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Url, GetUrlDto>().ReverseMap();
+    }
+}
--- shorturl.API/Program.cs
+++ shorturl.API/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using shorturl.API.Models;
 using shorturl.API.Repositories.Abstract;
 using shorturl.API.Repositories.Concrete;
+using shorturl.API.Services.Abstract;
+using shorturl.API.Services.Concrete;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +17,9 @@ builder.Services.AddDbContext<ShorturlDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ShorturlDb")));
 
 builder.Services.AddScoped<IUrlRepository, UrlRepository>();
+
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
